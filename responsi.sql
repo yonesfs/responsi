3e17b1617023f4a7ecc0ab64b5c6b449a1e18b4b
Yones Fernando <44739022+yonesfs@users.noreply.github.com>
2020-05-12 12:59:33 +0700

    Add files via upload

---
+++ responsi.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Bulan Mei 2020 pada 07.59
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `responsi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('yones', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data`
+--
+
+CREATE TABLE `data` (
+  `id` varchar(30) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `posisi` varchar(30) NOT NULL,
+  `gapok` int(11) NOT NULL,
+  `jamlembur` int(11) NOT NULL,
+  `tunjangan` int(11) NOT NULL,
+  `total_gaji` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`username`, `password`) VALUES
+('root@localhost', '123'),
+('yones', '123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
